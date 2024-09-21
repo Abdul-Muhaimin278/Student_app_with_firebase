c7152cad1cc3f3697452a77bd9fc9a151159a73c
Abdul-Muhaimin278 <abdulmuhaimin278@gmail.com>
2024-09-21 16:18:45 +0500

    commit

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBOpyBgplonyQcWckqdPx9O8h2iImnwoYs",
+    authDomain: "fir-demolition.firebaseapp.com",
+    projectId: "fir-demolition",
+    storageBucket: "fir-demolition.appspot.com",
+    messagingSenderId: "1080790339695",
+    appId: "1:1080790339695:web:3aa2ad38e0af480adabd9a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
