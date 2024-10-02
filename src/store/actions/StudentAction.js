816810b5559f9bd39ada5874019efab0a34a8e65
Abdul-Muhaimin278 <abdulmuhaimin278@gmail.com>
2024-10-02 11:50:39 +0500

    load more done

--- src/store/actions/StudentAction.js
+++ src/store/actions/StudentAction.js
@@ -3,64 +3,62 @@ import firebase, { db } from "../../config/firebase";
 
 //^ FETCH function
 export const fetchStudents = (uid, filter, lastVisible) => async (dispatch) => {
-	dispatch({
-		type: "FETCH_PENDING",
-	});
-	try {
-		const { searchName, searchRollNo, order } = filter;
+  dispatch({
+    type: "FETCH_PENDING",
+  });
 
-		let students = [];
-		let fetchRef = db
-			.collection("students")
-			.where("createdBy", "==", uid)
-			.orderBy("createdAt", order);
+  try {
+    const { searchName, searchRollNo, order } = filter;
 
-		if (searchName) {
-			fetchRef = fetchRef.where("name", "==", searchName);
-		}
-		if (searchRollNo) {
-			fetchRef = fetchRef.where("rollNo", "==", searchRollNo);
-		}
-		// let lastDoc = "";
-		await fetchRef
-			.limit(2)
-			.get()
-			.then((querySnapshot) => {
-				querySnapshot.forEach((doc) => {
-					students.push(doc.data());
-				});
-				// lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
-			});
-		console.log("students", students);
+    let students = [];
+    let fetchRef = db
+      .collection("students")
+      .where("createdBy", "==", uid)
+      .orderBy("createdAt", order);
 
-		if (lastVisible) {
-			await fetchRef.get().then((querySnapshot) => {
-				let lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
-				fetchRef.startAfter(lastVisible);
-				querySnapshot.forEach((doc) => {
-					students.push(doc.data());
-				});
+    if (searchName) {
+      fetchRef = fetchRef.where("name", "==", searchName);
+    }
+    if (searchRollNo) {
+      fetchRef = fetchRef.where("rollNo", "==", searchRollNo);
+    }
 
-				console.log("lastVisible", lastVisible);
-			});
-			dispatch({
-				type: "LOAD_MORE",
-				payload: { students, lastVisible },
-			});
-		} else {
-			dispatch({
-				type: "FETCH",
-				payload: { students, lastVisible },
-			});
-		}
-	} catch (error) {
-		console.error(error.message);
-		toast.error(error.message || "Unknown error occurred", { autoClose: 3000 });
-		dispatch({
-			type: "FETCH_ERROR",
-		});
-	}
+    let querySnapshot;
+    if (lastVisible) {
+      querySnapshot = await fetchRef
+        .startAfter(lastVisible)
+        .limit(2)
+        .get();
+    } else {
+      querySnapshot = await fetchRef.limit(2).get();
+    }
+
+    querySnapshot.forEach((doc) => {
+      students.push(doc.data());
+    });
+
+    const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+
+    if (lastVisible) {
+      dispatch({
+        type: "LOAD_MORE",
+        payload: { students, lastVisible: lastDoc },
+      });
+    } else {
+      dispatch({
+        type: "FETCH",
+        payload: { students, lastVisible: lastDoc },
+      });
+    }
+  } catch (error) {
+    console.error(error.message);
+    toast.error(error.message || "Unknown error occurred", { autoClose: 3000 });
+    dispatch({
+      type: "FETCH_ERROR",
+    });
+  }
 };
+
 
 //^ ADD STUDENT function
 export const addStudents = (uid, item) => async (dispatch) => {
